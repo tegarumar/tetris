8b61deb2a9d2bb634156fd1f1f755b777cb8beff
tegarumar <tegar.umar@gmail.com>
2024-10-14 15:28:27 +0700

    first commit

---
+++ src/app.js
@@ -0,0 +1,127 @@
+import Decorate from './components/decorate/index.vue'
+import Guide from './components/guide/index.vue'
+import Next from './components/next/index.vue'
+import Music from './components/music/index.vue'
+import Pause from './components/pause/index.vue'
+import Number from './components/number/index.vue'
+import Point from './components/point/index.vue'
+import Keyboard from './components/keyboard/index.vue'
+import Logo from './components/logo/index.vue'
+import Matrix from './components/matrix/index.vue'
+import Card from './components/card/index.vue'
+import { mapState } from 'vuex'
+import { transform, lastRecord, speeds, i18n, lan } from './unit/const'
+import { visibilityChangeEvent, isFocus } from './unit/'
+import states from './control/states'
+export default {
+  mounted() {
+    this.render()
+    window.addEventListener('resize', this.resize.bind(this), true)
+  },
+  data() {
+    return {
+      size: {},
+      w: document.documentElement.clientWidth,
+      h: document.documentElement.clientHeight,
+      filling: ''
+    }
+  },
+  components: {
+    Decorate,
+    Guide,
+    Next,
+    Music,
+    Pause,
+    Number,
+    Point,
+    Logo,
+    Keyboard,
+    Matrix,
+    Card
+  },
+  computed: {
+    pContent() {
+      return this.cur ? i18n.cleans[lan] : i18n.startLine[lan]
+    },
+    level: () => i18n.level[lan],
+    nextText: () => i18n.next[lan],
+    ...mapState([
+      'matrix',
+      'keyboard',
+      'music',
+      'pause',
+      'next',
+      'cur',
+      'speedStart',
+      'speedRun',
+      'startLines',
+      'clearLines',
+      'points',
+      'max',
+      'reset',
+      'drop'
+    ])
+  },
+  methods: {
+    render() {
+      let filling = 0
+      const size = (() => {
+        const w = this.w
+        const h = this.h
+        const ratio = h / w
+        let scale
+        let css = {}
+        if (ratio < 1.5) {
+          scale = h / 960
+        } else {
+          scale = w / 640
+          filling = (h - 960 * scale) / scale / 3
+          css = {
+            'padding-top': Math.floor(filling) + 42 + 'px',
+            'padding-bottom': Math.floor(filling) + 'px',
+            'margin-top': Math.floor(-480 - filling * 1.5) + 'px'
+          }
+        }
+        css[transform] = `scale(${scale})`
+        return css
+      })()
+      this.size = size
+      this.start()
+      this.filling = filling
+    },
+    resize() {
+      this.w = document.documentElement.clientWidth
+      this.h = document.documentElement.clientHeight
+      this.render()
+    },
+    start() {
+      if (visibilityChangeEvent) {
+        document.addEventListener(
+          visibilityChangeEvent,
+          () => {
+            states.focus(isFocus())
+          },
+          false
+        )
+      }
+
+      if (lastRecord) {
+        if (lastRecord.cur && !lastRecord.pause) {
+          const speedRun = this.$store.state.speedRun
+          let timeout = speeds[speedRun - 1] / 2
+          timeout =
+            speedRun < speeds[speeds.length - 1]
+              ? speeds[speeds.length - 1]
+              : speedRun
+          states.auto(timeout)
+        }
+
+        if (!lastRecord.cur) {
+          states.overStart()
+        }
+      } else {
+        states.overStart()
+      }
+    }
+  }
+}
---
+++ src/main.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import App from './App.vue'
+import store from './vuex/store'
+
+import './unit/const';
+import './control';
+import { subscribeRecord } from './unit';
+subscribeRecord(store);
+Vue.config.productionTip = false
+/* eslint-disable no-new */
+new Vue({
+  el: '#root',
+  render: h => h(App),
+  store: store
+})
